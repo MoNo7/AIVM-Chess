a95c7883f7899d9c3508c826fe6eb9606e221774
MoNo7 <mono_fl@yahoo.com>
2026-05-11 10:15:47 -0600

    Update relayer.js

--- api/relayer.js
+++ api/relayer.js
@@ -58,7 +58,7 @@ export default async function handler(req, res) {
         console.log("Current FEN from Contract:", gameData[2]);
         console.log("Move received from UI:", move);
         
-        const moveResult = game.move(moveObj); 
+        const moveResult = game.move(move); 
 
         if (!moveResult) {
             console.error("Validation Failed. FEN:", gameData.currentFEN, "Move Attempted:", moveObj);
