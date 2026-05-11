cf16a05ec4e830f75a4226084278b0f11ce20272
MoNo7 <mono_fl@yahoo.com>
2026-05-11 09:59:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -258,7 +258,7 @@ async function onDrop(source, target) {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ 
                     playerAddress: userAddress, 
-                    move: { source,target } // <--- Use .san here
+                    move: source + target 
                 })
             });
             
