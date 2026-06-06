720ac033b71d647723aba4fe2bd158227b06800a
MoNo7 <mono_fl@yahoo.com>
2026-06-06 15:52:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // --- Configuration ---
 const OWNER_ADDRESS = "0x4D36B31d4BFB957A5D816B0f420a9e755EFc6a2c";
-const CONTRACT_ADDRESS = "0x7CAc9C8d3D6B1e7b8ce470C7Ba308E91B08986cB";
+const CONTRACT_ADDRESS = "0x2C7D6d297dd1a925cd59D11dAC2e6bE2D552262c";
 const CONTRACT_ABI = [
     "function protocolOwner() view returns (address)",
     "function startMatch(string initialFEN) external payable",
